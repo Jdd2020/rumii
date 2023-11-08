c4cb7acb5462ba6f576381d3f556d5254a648c33
Olivia Terry <oliviaterry70@gmail.com>
2023-11-08 18:34:29 -0500

    skeleton for dash and chore list

---
+++ lib/views/chore_list_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ChoreListView extends StatefulWidget {
+  const ChoreListView({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ChoreListViewState createState() => _ChoreListViewState();
+}
+
+class _ChoreListViewState extends State<ChoreListView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Rumii")),
+        body: Container(
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: const Column(children: <Widget>[
+              Text('Hello'),
+            ])));
+  }
+}
---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DashboardViewState createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Rumii")),
+        body: Container(
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: const Column(children: <Widget>[
+              Text('Hello'),
+            ])));
+  }
+}
