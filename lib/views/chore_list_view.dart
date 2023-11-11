1fff3b449500a928a02fd81f45b02f057d02c4fb
huma27 <149990871+huma27@users.noreply.github.com>
2023-11-10 20:07:43 -0500

    chore_list_view.dart

--- lib/views/chore_list_view.dart
+++ lib/views/chore_list_view.dart
@@ -10,7 +10,6 @@ class ChoreListView extends StatefulWidget {
 }
 
 class _ChoreListViewState extends State<ChoreListView> {
-
   @override
   void initState() {
     super.initState();
@@ -19,23 +18,58 @@ class _ChoreListViewState extends State<ChoreListView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Rumii"),
-          automaticallyImplyLeading: false
+      appBar: AppBar(title: const Text("Rumii")),
+      body: Container(
+          padding: const EdgeInsets.all(10),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(children: <Widget>[
+            Text('Chore List'),
+            const SizedBox(height: 10),
+            Align(
+                alignment: Alignment.topRight,
+                child: InkWell(
+                    child: const Text(
+                      "+ New",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onTap: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const NewChore()))
+                        }))
+          ])),
+      bottomNavigationBar: CustomBottomNavigationBar(
+          currentRoute: '/chores',
+          onRouteChanged: (route) {
+            Navigator.of(context)
+                .pushNamed(route); // navigate to a different view
+          }),
+    );
+  }
+}
+
+//NewChore View
+class NewChore extends StatelessWidget {
+  const NewChore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            Text(' New Chore'),
+          ],
         ),
-        body: Container(
-            padding: const EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: const Column(children: <Widget>[
-              Text('Chore List'),
-            ])),
-            bottomNavigationBar: CustomBottomNavigationBar(
-              currentRoute: '/chores', 
-              onRouteChanged: (route) {
-                Navigator.of(context).pushNamed(route); // navigate to a different view
-              } 
-            ),
-      );
+      ),
+    );
   }
 }
