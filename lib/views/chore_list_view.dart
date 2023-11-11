5b954a36b3df7290b1a61fb2a4ad56dcb47bf35f
huma27 <149990871+huma27@users.noreply.github.com>
2023-11-11 00:43:58 -0500

    chore_list_view.dart

--- lib/views/chore_list_view.dart
+++ lib/views/chore_list_view.dart
@@ -41,7 +41,7 @@ class _ChoreListViewState extends State<ChoreListView> {
                               context,
                               MaterialPageRoute(
                                   builder: (context) => const NewChore()))
-                        }))
+                        })),
           ])),
       bottomNavigationBar: CustomBottomNavigationBar(
           currentRoute: '/chores',
@@ -64,11 +64,131 @@ class NewChore extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: <Widget>[
-            Text(' New Chore'),
-          ],
-        ),
+        child: Column(children: <Widget>[
+          Text(' New Chore'),
+          const SizedBox(height: 20),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            InkWell(
+                child: const Text('Cancel',
+                    style: TextStyle(
+                      fontSize: 16,
+                    )),
+                onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ChoreListView()))
+                    }),
+            InkWell(
+                child: const Text('Save',
+                    style: TextStyle(
+                      fontSize: 16,
+                    )),
+                onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ChoreListView()))
+                    }),
+          ]),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Name your Chore'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Assign User'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Due Date'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            width: 1500,
+            child: DropdownButton<String>(
+              items: [
+                DropdownMenuItem<String>(
+                  value: 'Daily',
+                  child: Text('Daily'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'weekly',
+                  child: Text('weekly'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'bi-weekly',
+                  child: Text('bi-weekly'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'monthly',
+                  child: Text('monthly'),
+                ),
+              ],
+              onChanged: (String? value) {
+                //hanlde the selected
+                return;
+              },
+              hint: Text('Repetition'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Reminder'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Note'),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SizedBox(
+            width: 1500,
+            child: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Points'),
+            ),
+          ),
+        ]),
       ),
     );
   }
