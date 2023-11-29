6e6baa689f7b4ba1ce56109c63f7a57d48c1b725
huma27 <149990871+huma27@users.noreply.github.com>
2023-11-29 11:38:21 -0500

    shopping_list_view.dart

--- lib/views/Shopping/shopping_list_view.dart
+++ lib/views/Shopping/shopping_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rumii/viewmodels/shopping_list_view_model.dart';
+import 'package:rumii/views/Shopping/new_item_view.dart';
 import 'package:rumii/views/widgets/custom_bottom_navigation_bar.dart';
 
 class ShoppingListView extends StatefulWidget {
@@ -10,7 +13,6 @@ class ShoppingListView extends StatefulWidget {
 }
 
 class _ShoppingListViewState extends State<ShoppingListView> {
-
   @override
   void initState() {
     super.initState();
@@ -19,27 +21,65 @@ class _ShoppingListViewState extends State<ShoppingListView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Image.asset('assets/images/rumii-logo.png', height: 28.00, width: 70.00), //const Text("Rumii"),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-        ),
-        body: Container(
-            padding: const EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: const Column(
+      appBar: AppBar(
+        title: Image.asset('assets/images/rumii-logo.png',
+            height: 28.00, width: 70.00), //const Text("Rumii"),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(20),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-              SizedBox(height: 10),
-              Text('Shopping List', style: (TextStyle(fontSize: 26, fontWeight: FontWeight.bold,))),
-            ])),
-            bottomNavigationBar: CustomBottomNavigationBar(
-              currentRoute: '/shopping_list', 
-              onRouteChanged: (route) {
-                Navigator.of(context).pushNamed(route); // navigate to a different view
-              } 
-            ),
-      );
+                const SizedBox(height: 10),
+                const Text('Shopping List',
+                    style: (TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ))),
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: InkWell(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.pink,
+                      ),
+                      padding: EdgeInsets.all(3),
+                      child: const Text(
+                        "+",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ChangeNotifierProvider(
+                                  create: (context) => ShoppingListViewModel(),
+                                  child: const NewItem(),
+                                )),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // add the list
+              ])),
+      bottomNavigationBar: CustomBottomNavigationBar(
+          currentRoute: '/shopping_list',
+          onRouteChanged: (route) {
+            Navigator.of(context)
+                .pushNamed(route); // navigate to a different view
+          }),
+    );
   }
 }
