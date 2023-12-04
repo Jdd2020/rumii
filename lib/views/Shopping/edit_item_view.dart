0aa4cbe1ed1fc6971cef114bab7020a3497dfcf6
huma27 <149990871+huma27@users.noreply.github.com>
2023-12-04 17:15:18 -0500

    edit_item_view.dart

--- lib/views/Shopping/edit_item_view.dart
+++ lib/views/Shopping/edit_item_view.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:rumii/viewmodels/shopping_list_view_model.dart';
-import 'package:provider/provider.dart';
+import 'package:rumii/models/shop_model.dart';
 
 class EditItem extends StatefulWidget {
-  const EditItem({Key? key}) : super(key: key);
+  final Shop shop;
+  const EditItem({Key? key, required this.shop}) : super(key: key);
 
   @override
   _EditItemState createState() => _EditItemState();
 }
 
 class _EditItemState extends State<EditItem> {
+  late TextEditingController itemController;
+  late TextEditingController quantityController;
+  late TextEditingController typeController;
+  late TextEditingController notesController;
+
+  @override
+  void initState() {
+    super.initState();
+    itemController = TextEditingController(text: widget.shop.name);
+    quantityController =
+        TextEditingController(text: widget.shop.quantity.toString());
+    notesController = TextEditingController(text: widget.shop.notes);
+    typeController = TextEditingController(text: widget.shop.type);
+  }
+
+  @override
+  void dispose() {
+    // Dispose controllers to avoid memory leaks
+    itemController.dispose();
+    quantityController.dispose();
+    typeController.dispose();
+    notesController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,20 +66,57 @@ class _EditItemState extends State<EditItem> {
                 ),
               ),
               TextButton(
-                  onPressed: () {
-                    // Add logic for "Save" button here
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Save',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black,
-                      ))),
+                onPressed: () {
+                  //save
+                  Navigator.pop(context);
+                },
+                child: const Text('Save',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.black,
+                    )),
+              ),
             ]),
-            //add other widgets
+            // editable text fields
+            const SizedBox(height: 20),
+            buildEditableTextField("Item", itemController),
+            buildEditableTextField("Quantity", quantityController),
+            buildEditableTextField("Type", typeController),
+            buildEditableTextField("Notes", notesController),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildEditableTextField(
+      String label, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(
+          width: 1250,
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
     );
   }
 }
