c968ead85f93c050a53376c934086513d7a0e616
huma27 <149990871+huma27@users.noreply.github.com>
2023-12-06 14:59:53 -0500

    added dropdown menu for users and delete button. Adjusted the box sizes

--- lib/views/Shopping/edit_item_view.dart
+++ lib/views/Shopping/edit_item_view.dart
@@ -6,8 +6,10 @@ class EditItem extends StatefulWidget {
   final String user;
   final ShopViewModel shop;
   final String lastItem;
+  final List<String> householdMembers = ['Henry', 'Josh', 'Billy'];
 
-  const EditItem({Key? key, 
+  EditItem({
+    Key? key,
     required this.user,
     required this.shop,
     required this.lastItem,
@@ -19,24 +21,29 @@ class EditItem extends StatefulWidget {
 
 class _EditItemState extends State<EditItem> {
   late TextEditingController itemController;
+  late TextEditingController assignUserController;
   late TextEditingController quantityController;
   late TextEditingController typeController;
   late TextEditingController notesController;
+  String selectedAssignee = '';
 
   @override
   void initState() {
     super.initState();
     itemController = TextEditingController(text: widget.shop.name);
+    assignUserController = TextEditingController(text: widget.user);
     quantityController =
         TextEditingController(text: widget.shop.quantity.toString());
     notesController = TextEditingController(text: widget.shop.notes);
     typeController = TextEditingController(text: widget.shop.type);
+    selectedAssignee = widget.user;
   }
 
   @override
   void dispose() {
     // Dispose controllers to avoid memory leaks
     itemController.dispose();
+    assignUserController.dispose();
     quantityController.dispose();
     typeController.dispose();
     notesController.dispose();
@@ -46,52 +53,67 @@ class _EditItemState extends State<EditItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: EdgeInsets.only(top: 50.0),
-                child: Text(
-                  'Edit Item',
-                  style: TextStyle(
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Text(
+                    'Edit Item',
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text(
-                  'Cancel',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  //save
-                  Navigator.pop(context);
-                },
-                child: const Text('Save',
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text(
+                    'Cancel',
                     style: TextStyle(
                       fontSize: 16.0,
                       color: Colors.black,
-                    )),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //save
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Save',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      )),
+                ),
+              ]),
+              // Editable text fields
+              const SizedBox(height: 20),
+              buildEditableTextField("Item", itemController),
+              buildEditableTextField("Assign User", assignUserController),
+              buildEditableTextField("Quantity", quantityController),
+              buildEditableTextField("Type", typeController),
+              buildEditableTextField("Notes", notesController),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle the delete logic
+                  },
+                  child: const Text("Delete"),
+                ),
               ),
-            ]),
-            // editable text fields
-            const SizedBox(height: 20),
-            buildEditableTextField("Item", itemController),
-            buildEditableTextField("Quantity", quantityController),
-            buildEditableTextField("Type", typeController),
-            buildEditableTextField("Notes", notesController),
-          ],
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
@@ -102,28 +124,56 @@ class _EditItemState extends State<EditItem> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const SizedBox(height: 6),
         Text(
           label,
           style: const TextStyle(
             fontWeight: FontWeight.normal,
           ),
         ),
-        SizedBox(
-          width: 1250,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-            ),
-            child: TextField(
-              controller: controller,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
+        const SizedBox(height: 2),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.98,
+          height: 50,
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: label == "Assign User"
+                    ? DropdownButtonFormField<String>(
+                        value: controller.text,
+                        items: widget.householdMembers.map((String member) {
+                          return DropdownMenuItem<String>(
+                            value: member,
+                            child: Text(member),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          if (newValue != null) {
+                            setState(() {
+                              controller.text = newValue;
+                            });
+                          }
+                        },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      )
+                    : TextField(
+                        controller: controller,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
               ),
-            ),
+            ],
           ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 10),
       ],
     );
   }
